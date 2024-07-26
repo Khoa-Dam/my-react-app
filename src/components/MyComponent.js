0aafc15a62b632d1c0ffdd376a4ef88272938079
KhoaND hoc code <damngockhoa0703@gmail.com>
2024-07-26 14:55:09 +0700

    continue

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -5,14 +5,19 @@
 import React from 'react';
 
 class MyComponent extends React.Component {
+
+  state = {
+    name: "Khoa",
+    address: "NguoiVN",
+    age: 16,
+  };
+
   //JSX
   render(){
     return (
-      <div>my first component
+      <div>
 
-        {
-          Math.random()
-        }
+        My name is {this.state.name} and I'm from {this.state.address}
       </div>
     );
   }
