ba3528ada8661cc3eff07541996e53007bd8b523
KhoaND hoc code <damngockhoa0703@gmail.com>
2024-07-26 19:34:28 +0700

    Nesting Component

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,55 +1,14 @@
 // class component
 // function component
 
-
 import React from 'react';
-
+import UserInfor from "./UserInfor.js"
 class MyComponent extends React.Component {
-
-  state = {
-    name: "Khoa",
-    address: "NguoiVN",
-    age: 16,
-  };
-
-  handleClick(event) {
-    console.log(">> Click me my button");
-    // console.log("Random", Math.floor(Math.random()*100 + 1));
-    
-    //merge State => react class
-    this.setState({
-      name: "Khoa Đàm",
-      age: Math.floor(Math.random()*100 + 1)
-    })
-  }
-
-  handleOnMoverOver(event){
-    // console.log(event.pageX);
-  }
-  handleOnChangeInput = (event) => {
-    this.setState({
-      name : event.target.value,
-    })
-  }
-
-  handleOnSubmit(event) {
-    event.preventDefault();
-    console.log(this.state);
-  }
   //JSX
   render(){
     return (
       <div>
-
-        My name is {this.state.name} and I'm {this.state.age}
-        {/* <button onClick={(event) =>this.handleClick(event)}>Click me</button> */}
-        <form onSubmit={(event) => this.handleOnSubmit(event)}>
-          <input 
-          type ="text"
-            onChange = {(event) => {this.handleOnChangeInput(event)}}
-          />
-          <button>Submit</button>
-        </form>
+        <UserInfor/>
       </div>
     );
   }
