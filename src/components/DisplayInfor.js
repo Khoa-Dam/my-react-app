c0afb2b06923b712e44e7f635a93bd25dd49ca1a
KhoaND hoc code <damngockhoa0703@gmail.com>
2024-07-28 11:05:06 +0700

    Image

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,5 +1,7 @@
 import React from "react";
 import './DisplayInfor.scss';
+import logo from './../logo.svg';
+
 class DisplayInfor extends React.Component {
     state = {
         isShowListUser: true
@@ -15,6 +17,7 @@ class DisplayInfor extends React.Component {
         //props => viết tắt properties
         return (
             <div className='display-infor-container'>
+                <img src={logo} />
                 <div>
                     <span onClick={() => { this.handleShowHide() }}>
                         {this.state.isShowListUser === true ? "Hide list user:" : "Show list user:"}
@@ -25,7 +28,7 @@ class DisplayInfor extends React.Component {
                         {listUsers.map((user) => {
                             return (
                                 <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
-                                    <div style={{ color: 'yellow', paddingTop: '50px' }}>My name's {user.name} </div>
+                                    <div >My name's {user.name} </div>
                                     <div>My age's {user.age}</div>
                                     <hr />
                                 </div>
