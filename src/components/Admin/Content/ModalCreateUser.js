768cf079698085efd5a8574574c1d174da633a1a
KhoaND hoc code <damngockhoa0703@gmail.com>
2024-08-05 20:52:34 +0700

    API Services Customize Axios

--- src/components/Admin/Content/ModalCreateUser.js
+++ src/components/Admin/Content/ModalCreateUser.js
@@ -3,11 +3,8 @@ import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import { BsPlusSquareFill } from "react-icons/bs";
-import axios from 'axios';
 import { toast } from 'react-toastify';
-
-
-
+import { postCreateNewUser } from "../../../services/apiService";
 
 const ModalCreateUser = (props) => {
     const { show, setShow } = props;
@@ -57,25 +54,16 @@ const ModalCreateUser = (props) => {
             return;
         }
 
-        //call apis
-        //submit data
-        const data = new FormData();
-        data.append('email', email);
-        data.append('password', password);
-        data.append('username', username);
-        data.append('role', role);
-        data.append('userImage', image);
-
-        let res = await axios.post('http://localhost:8081/api/v1/participant', data);
-        console.log(">>check res: ", res.data)
-        if (res.data && res.data.EC === 0) {
-            toast.success(res.data.EM);
+        //call api
+        let data = await postCreateNewUser(email, password, username, role, image);
+        console.log("component res: ", data);
+        if (data && data.EC === 0) {
+            toast.success(data.EM);
             handleClose();
         }
-        if (res.data && res.data.EC !== 0) {
-            toast.error(res.data.EM);
+        if (data && data.EC !== 0) {
+            toast.error(data.EM);
         }
-
 
     }
     return (
